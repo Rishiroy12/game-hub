68f6308e681f45fb6eb328f9d64d162bba993430
Rishi Kumar <rishikrbxr9@gmail.com>
2024-07-07 01:39:17 +0530

    Refactor:remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,8 +11,8 @@ game:Game;
 }
 const GameCard = ({game} : Props) => {
   return (
-    <Card width = '250px' borderRadius={10} overflow='hidden'>
-      <Image src={  game.background_image }/>
+    <Card >
+      <Image src={ game.background_image }/>
     <CardBody>
       <Heading fontSize={'2xl'}>{game.name}</Heading>
       <HStack justifyContent='space-between'>
