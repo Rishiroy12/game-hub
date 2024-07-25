aa9fbef28a33c96e0f63b7592f551ca495fd92a9
Rishi Kumar <rishikrbxr9@gmail.com>
2024-07-25 21:14:21 +0530

    Update page Title

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,7 +14,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
